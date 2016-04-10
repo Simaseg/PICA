348566587b273eb1011957847e0205aa41d66cc7
Enzo <e.menegazzi@hotmail.fr>
2016-04-10 16:28:09 -0400

    added enter name scene added highscores scene added timer between scenes added menu into entername into level2 transitions

---
+++ Assets/_SCRIPT/Highscores/EnterNameManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class EnterNameManager : MonoBehaviour {
+
+    [Header("Important Values")]
+    
+    // the visual text showing the name
+    public Text nameDisplay;
+    // the maximum number of characters the name can have
+    public int maxLength = 5;
+    
+    [Header("Divers")]
+
+    // the selected object when entering the scene
+    public GameObject firstSelectedObject;
+
+    private string _name;
+    private int _actualLength = 0;
+
+    void Start() {
+        EventSystem.current.SetSelectedGameObject(firstSelectedObject);
+    }
+
+    void Update() {
+        Debug.Log(_actualLength);
+    }
+
+    /// <summary>
+    /// updates the text value
+    /// </summary>
+	void UpdateName() {
+        nameDisplay.text = _name;
+    }
+
+    /// <summary>
+    /// BUTTON: adds a letter to the name
+    /// updates the name
+    /// </summary>
+    public void AddLetter(string _letter) {
+        if (_actualLength < maxLength) {
+            _name = _name + _letter;
+
+            _actualLength++;
+        }
+
+        UpdateName();
+    }
+
+    /// <summary>
+    /// BUTTON: removes the last letter of the name
+    /// updates the name
+    /// </summary>
+    public void RemoveLetter() {
+        if (_actualLength > 0) {
+            _name = _name.Remove(_name.Length - 1);
+
+            _actualLength--;
+        }
+
+        UpdateName();
+    }
+
+    /// <summary>
+    /// saves the name and loads the next scene
+    /// </summary>
+    public void Continue(string _sceneName) {
+        LeaderboardManager.s_playerName = _name;
+
+        SceneManager.LoadScene(_sceneName);
+    }
+}
---
+++ Assets/_SCRIPT/Highscores/SaveScores.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SaveScores : MonoBehaviour {
+
+    public Text saveResult;
+
+    private string _saveScoreUrl = "http://e.menegazzi.free.fr/Pica_01/SaveScore.php?";
+
+    IEnumerator SaveScore(string name, float score) {
+        string post_url = _saveScoreUrl + "name=" + WWW.EscapeURL(name) + "&score=" + score;
+
+        // Post the URL to the site and create a download object to get the result.
+        WWW hs_post = new WWW(post_url);
+        yield return hs_post; // Wait until the download is done
+
+        if (hs_post.error != null) {
+            print("There was an error posting the high score: " + hs_post.error);
+            saveResult.text = "Il y a eu une erreur.";
+        }
+    }
+
+    public void Save() {
+        StartCoroutine(SaveScore("Test01", Time.timeSinceLevelLoad));
+        saveResult.text = "Le score est sauvegardé.";
+    }
+}
