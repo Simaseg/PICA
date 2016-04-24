90be573a6f5e8cdbc4d97c1deaf56eb2f4d64c29
Simon Seguin <sim.seguin@gmail.com>
2016-04-24 13:59:28 -0400

    no message

--- Assets/PicaScore.cs
+++ Assets/PicaScore.cs
@@ -5,22 +5,32 @@ using UnityEngine.UI;
 public class PicaScore : MonoBehaviour {
 
 	public int count;
+	public GameObject exitSprite;
+	public GameObject blockedSprite;
 
 	// Use this for initialization
 	void Start () {
 
 		count = 0;
+		exitSprite.SetActive(false);
+		blockedSprite.SetActive(true);
 	
 	}
 
 	// Update is called once per frame
-	void Update () {
 
+	void Update () {
+		if (count == 5) {
+			//Application.LoadLevel ("Level1");
+			Cursor.visible = true;
+
+			exitSprite.SetActive(true);
+			blockedSprite.SetActive(false);
+		}
 		if (count == 6) {
 			Application.LoadLevel ("Level1");
-			Cursor.visible = true;
-	
 		}
+
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
--- Assets/PicaScoreLevel3.cs
+++ Assets/PicaScoreLevel3.cs
@@ -4,21 +4,33 @@ using System.Collections;
 public class PicaScoreLevel3 : MonoBehaviour {
 
 	public int count;
+	public GameObject exitSprite;
+	public GameObject blockedSprite;
 	
 	// Use this for initialization
 	void Start () {
 		
 		count = 0;
+		exitSprite.SetActive(false);
+		blockedSprite.SetActive(true);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+			if (count == 5) {
+				//Application.LoadLevel ("Level1");
+				Cursor.visible = true;
+				
+				exitSprite.SetActive(true);
+				blockedSprite.SetActive(false);
+			}
+
 		if (Input.GetKey(KeyCode.Escape))
-		{
-			Application.Quit();
-		}
+			{
+				Application.Quit();
+			}
 	}
 	
 	void OnTriggerEnter2D (Collider2D other)
